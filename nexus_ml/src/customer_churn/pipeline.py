d9d7d11f95a72db0e675486ba3c4d4a965b24410
Sagar Sambhwani <sagar.2001.a20@gmail.com>
2026-08-12 20:15:26 +0530

    refactor(customer-churn): update pipeline namespace imports

--- nexus_ml/src/customer_churn/pipeline.py
+++ nexus_ml/src/customer_churn/pipeline.py
@@ -4,8 +4,8 @@ from typing import Dict, Any
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score
-from config import RANDOM_SEED
-from src.common.base_model import BasePipeline
+from nexus_ml.config import RANDOM_SEED
+from nexus_ml.src.common.base_model import BasePipeline
 
 class CustomerChurnPipeline(BasePipeline):
     def __init__(self):
