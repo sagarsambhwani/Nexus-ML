e1c145b7d26b1f2e036ce10af5af0a7fa3ca909f
Sagar Sambhwani <sagar.2001.a20@gmail.com>
2026-08-12 20:16:03 +0530

    refactor(customer-segmentation): update pipeline namespace imports

--- nexus_ml/src/customer_segmentation/pipeline.py
+++ nexus_ml/src/customer_segmentation/pipeline.py
@@ -4,8 +4,8 @@ from typing import Dict, Any
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-from config import RANDOM_SEED
-from src.common.base_model import BasePipeline
+from nexus_ml.config import RANDOM_SEED
+from nexus_ml.src.common.base_model import BasePipeline
 
 class CustomerSegmentationPipeline(BasePipeline):
     def __init__(self):
